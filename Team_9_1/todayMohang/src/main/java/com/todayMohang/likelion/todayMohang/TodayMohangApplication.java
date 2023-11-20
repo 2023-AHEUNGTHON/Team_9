5d0ff382bcf31ee065fb6ab81f95dfd7b013fb62
Jeoongu <ms235280@naver.com>
2023-11-20 21:15:15 +0900

    add : basic

---
+++ Team_9_1/todayMohang/src/main/java/com/todayMohang/likelion/todayMohang/TodayMohangApplication.java
@@ -0,0 +1,13 @@
+package com.todayMohang.likelion.todayMohang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodayMohangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodayMohangApplication.class, args);
+	}
+
+}
---
+++ Team_9_1/todayMohang/src/test/java/com/todayMohang/likelion/todayMohang/TodayMohangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todayMohang.likelion.todayMohang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodayMohangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
