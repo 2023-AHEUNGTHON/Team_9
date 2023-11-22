cd9c58b523167dcd49eeb06c2e381b61550f1ad2
Jiyun82 <202110918@sangmyung.kr>
2023-11-23 04:00:14 +0900

    Feat: api 설명 추가

--- Team_9_1/todayMohang/src/main/java/com/todayMohang/likelion/todayMohang/controller/PostController.java
+++ Team_9_1/todayMohang/src/main/java/com/todayMohang/likelion/todayMohang/controller/PostController.java
@@ -7,7 +7,9 @@ import com.todayMohang.likelion.todayMohang.dto.PostResponseDto;
 import com.todayMohang.likelion.todayMohang.dto.UserPostResponseDto;
 import com.todayMohang.likelion.todayMohang.service.PostServiceImpl;
 import com.todayMohang.likelion.todayMohang.service.UserService;
-import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -30,7 +32,13 @@ public class PostController {
     @Autowired
     UserService userService;
 
-    @Operation(summary = "", description = "")
+    @ApiOperation(value = "행사 생성 api", notes = "토큰 필요")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "대학 인증 필요"),
+            @ApiResponse(code = 404, message = "유저 정보 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
     @PostMapping("/post/create")
     public ResponseEntity<?> createPost(@RequestPart(value = "data") PostRequestDto postRequestDto,
                                         @RequestPart(value = "file", required = false) List<MultipartFile> files,
@@ -46,35 +54,47 @@ public class PostController {
                 }
                 return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
             }
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @Operation(summary = "", description = "")
+    @ApiOperation(value = "행사 전체 조회 api", notes = "로그인 상태에서만 토큰 필요, 날짜는 Epoch timestamp")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 404, message = "유저 정보 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
     @GetMapping("/posts")
     public ResponseEntity<List<PostResponseDto>> getPosts(Authentication authentication){
         try {
+            List<Post> postList = postService.findAll();
+            List<PostResponseDto> postResponseDtoList = postList.stream().map(PostResponseDto::new).collect(Collectors.toList());
+            if(authentication == null) {
+                return ResponseEntity.ok().body(postResponseDtoList);
+            }
             String email = authentication.getName();
             Optional<User> userOptional = userService.findByEmail(email);
             if(userOptional.isPresent()) {
                 User user = userOptional.get();
-                List<Post> postList = postService.findAll();
                 /**북마크 리스트 가져오기 -> 게시글 리스트랑 비교(postList.contains())해서 북마크인 애들은 dto에서 bookmark = true로 되게 하기*/
-                List<PostResponseDto> postResponseDtoList = postList.stream().map(PostResponseDto::new).collect(Collectors.toList());
                 return ResponseEntity.ok().body(postResponseDtoList);
-            } else {
-
             }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @Operation(summary = "", description = "")
+    @ApiOperation(value = "내 행사 조회 api", notes = "토큰 필요, 날짜는 Epoch timestamp")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 404, message = "유저 정보 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
     @GetMapping("/mypage/events")
     public ResponseEntity<List<UserPostResponseDto>> getPostsByUser(Authentication authentication){
         try {
@@ -85,14 +105,21 @@ public class PostController {
                 List<UserPostResponseDto> responseDtoList = postList.stream().map(UserPostResponseDto::new).collect(Collectors.toList());
                 return ResponseEntity.ok().body(responseDtoList);
             }
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @Operation(summary = "", description = "")
+    @ApiOperation(value = "행사 수정 api", notes = "토큰 필요")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 400, message = "존재하지 않는 post id"),
+            @ApiResponse(code = 403, message = "접근 권한 없음"),
+            @ApiResponse(code = 404, message = "유저 정보 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
     @PutMapping("/post/update/{postId}")
     public ResponseEntity<?> updatePost(@PathVariable("postId") Long postId,
                                         @RequestPart(value = "data") PostRequestDto postRequestDto,
@@ -121,7 +148,14 @@ public class PostController {
         return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    @Operation(summary = "", description = "")
+    @ApiOperation(value = "행사 삭제 api", notes = "토큰 필요")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 400, message = "존재하지 않는 post id"),
+            @ApiResponse(code = 403, message = "접근 권한 없음"),
+            @ApiResponse(code = 404, message = "유저 정보 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
     @DeleteMapping("/post/delete/{postId}")
     public ResponseEntity<?> deletePost(@PathVariable("postId") Long postId,
                                         Authentication authentication) {
@@ -140,7 +174,7 @@ public class PostController {
                 }
                 return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
             }
-            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         } catch (Exception e) {
             e.printStackTrace();
         }
